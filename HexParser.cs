aefb609c7882cd1ff5fb3cc27d98fbf53cfe7628
Ruhlenko <ruhlenko@gmail.com>
2015-07-15 16:28:02 +0500

    Namespace fixed

--- HexParser.cs
+++ HexParser.cs
@@ -2,7 +2,7 @@
 using System.Globalization;
 using System.Text;
 
-namespace hcGate
+namespace hcClient
 {
     static class HexParser
     {
--- hcCommand.cs
+++ hcCommand.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace hcGate
+namespace hcClient
 {
     class hcCommand : EventArgs
     {
