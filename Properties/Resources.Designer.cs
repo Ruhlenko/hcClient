1ba41619d5042802158c22ccf792683b168e3c4b
Ruhlenko <ruhlenko@gmail.com>
2015-09-28 16:58:58 +0500

    Added Fan icon

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -143,6 +143,26 @@ namespace hcClient.Properties {
         /// <summary>
         ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap fan_24_0 {
+            get {
+                object obj = ResourceManager.GetObject("fan_24_0", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap fan_24_1 {
+            get {
+                object obj = ResourceManager.GetObject("fan_24_1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap fan_speed_24_0 {
             get {
                 object obj = ResourceManager.GetObject("fan_speed_24_0", resourceCulture);
