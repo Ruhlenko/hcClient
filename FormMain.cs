8ceabb8878f0ce58ecb200377d0c56b5d38a3bea
Ruhlenko <ruhlenko@gmail.com>
2015-10-07 17:19:33 +0500

    Удаление неиспользуемых using

--- FormMain.cs
+++ FormMain.cs
@@ -1,11 +1,8 @@
 ﻿using hcClient.ui;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
 using System.Drawing.Drawing2D;
-using System.Text;
 using System.Windows.Forms;
 
 namespace hcClient
